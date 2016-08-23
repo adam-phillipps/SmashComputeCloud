eb358748f1d895e13ffed0d2c1fbce1d424b44e4
adam-phillipps <adam.phillipps@gmail.com>
2016-08-22 20:11:31 -0600

    fix compute_controller

--- administrator.rb
+++ administrator.rb
@@ -3,11 +3,20 @@ Aws.use_bundled_cert!
 require 'httparty'
 require 'syslog/logger'
 require 'logger'
+require 'json'
 require 'byebug'
 
 module Administrator
+  def boot_time
+    Time.now.to_i # comment the code below for development mode
+    # @instance_boot_time ||=
+    #   ec2.describe_instances(instance_ids:[self_id]).
+    #     reservations[0].instances[0].launch_time.to_i
+  end
+
   def self_id
-    @id ||= HTTParty.get('http://169.254.169.254/latest/meta-data/instance-id')
+    'test-id' # comment the below line for development mode
+    # @id ||= HTTParty.get('http://169.254.169.254/latest/meta-data/instance-id')
   end
 
   def poller(board)
@@ -18,6 +27,22 @@ module Administrator
         logger.error("There isn't a '#{board}' poller available...\n#{e}")
       end
     end
+  end
+
+  def backlog_poller
+    @backlog_poller ||= Aws::SQS::QueuePoller.new(backlog_address)
+  end
+
+  def wip_poller
+    @wip_poller ||= Aws::SQS::QueuePoller.new(wip_address)
+  end
+
+  def finished_poller
+    @finished_poller ||= Aws::SQS::QueuePoller.new(finished_address)
+  end
+
+  def counter_poller
+    @counter_poller ||= Aws::SQS::QueuePoller.new(bot_counter_address)
   end
 
   def sqs
@@ -31,8 +56,15 @@ module Administrator
     )
   end
 
+  def kinesis
+    @kinesis ||= Aws::Kinesis::Client.new(
+      region: region,
+      credentials: creds,
+    )
+  end
+
   def errors
-    $errors ||= { ruby: [], scraper: [], workflow: [] }
+    $errors ||= { ruby: [], workflow: [] }
   end
 
   def backlog_address
@@ -55,6 +87,14 @@ module Administrator
     @needs_attention_address ||= ENV['NEEDS_ATTENTION_ADDRESS']
   end
 
+  def status_address
+    @status_address ||= ENV['STATUS_ADDRESS']
+  end
+
+  def status_stream_name
+    @status_stream_name ||= ENV['STATUS_STREAM_NAME']
+  end
+
   def region
     @region ||= ENV['AWS_REGION']
   end
@@ -68,10 +108,19 @@ module Administrator
   end
 
   def get_count(board)
-    sqs.get_queue_attributes(
+    update_status_checks()
+    count = sqs.get_queue_attributes(
       queue_url: board,
       attribute_names: ['ApproximateNumberOfMessages']
     ).attributes['ApproximateNumberOfMessages'].to_f
+
+    message = {
+     instanceID: self_id,
+     type: 'SitRep',
+     content: 'Count',
+     extraInfo: { board => count }
+    }.to_json
+    send_status_to_stream(self_id, message)
   end
 
   def filename
@@ -125,23 +174,140 @@ module Administrator
 
   def notification_of_death
     blame = errors.sort_by(&:reverse).last.first
+    message = {
+     instanceID: self_id,
+     type: 'status_update',
+     content: 'Dying',
+     extraInfo: blame
+    }.to_json
+
+    update_status_checks(self_id, message)
+    sqs.send_message(
+      {
+        queue_url: status_address,
+        message_body: message
+      }.to_json
+    )
+    sqs.send_message(
+      queue_url: needs_attention_address,
+      message_body: message
+    )
     logger.info("The cause for the shutdown is #{blame}")
   end
 
-  private
-  def backlog_poller
-    @backlog_poller ||= Aws::SQS::QueuePoller.new(backlog_address)
+  def update_status_checks(ids, status)
+    for_each_id_send_message_to(
+      status_address,
+      ids,
+      status
+    )
+    send_status_to_stream(ids, status)
   end
 
-  def wip_poller
-    @wip_poller ||= Aws::SQS::QueuePoller.new(wip_address)
+  def send_status_to_stream(ids, status)
+    byebug
+    ids = ids.kind_of?(Array) ? ids : [ids.to_s]
+    it_worked = false
+
+    until it_worked
+      begin
+        if ids.count == 1
+          resp = kinesis.put_record(
+            stream_name: status_stream_name,
+            data: status,
+            partition_key: ids.first
+          )
+
+          unless resp[:sequence_number] && resp[:shard_id]
+            send_status_to_stream(ids, status)
+          end
+
+          it_worked = true
+        elsif ids.count > 1
+          resp = kinesis.put_records(
+            stream_name: status_stream_name,
+            records: ids.map { |id| { data: status, partition_key: id } }
+          )
+
+          if resp.failed_count > 0
+            logger.info "Failed stream update count: #{resp.failed_record_count}\n" +
+              "Error(s):\n#{resp.records.map(&:error_message).join("\n")}"
+          end
+
+          @last_sequence_number = resp.records.map(&:sequence_number).sort.last
+
+          failed_resps = resp.records.select { |r| r.body unless r.successful? }
+          # TODO:
+          # get the ids and send_status_to_stream again
+          logger.info "failed to start: #{failed_resps.join("\n")}"
+
+          it_worked = true
+        end
+        it_worked
+      rescue ResourceNotFoundException => e
+        create_stream
+      end
+    end
   end
 
-  def finished_poller
-    @finished_poller ||= Aws::SQS::QueuePoller.new(finished_address)
+  def create_stream(opts = {})
+    begin
+      config = {
+        stream_name: status_stream_name,
+        shard_count: 1
+      }.merge(opts)
+
+      @status_stream ||= kinesis.create_stream(config)
+      kinesis.wait_until(:stream_exists, stream_name: config[:stream_name])
+
+      logger.info "Created stream named #{config[:stream_name]}"
+    rescue Exception => e
+      if e == ResourceInUseException
+        logger.info "Stream already created"
+      else
+        errors[:ruby] << [e.message, e.backtrace].join("\n")
+      end
+    end
   end
 
-  def counter_poller
-    @counter_poller ||= Aws::SQS::QueuePoller.new(bot_counter_address)
+  def status_stream
+    @status_stream
+  end
+
+  def add_to_working_bots_count(ids)
+    for_each_id_send_message_to(
+      bot_counter_address,
+      ids,
+      Time.now
+    )
+  end
+
+  def for_each_id_send_message_to(board, ids, message)
+    ids.each do |id|
+      sqs.send_message(
+        queue_url: board,
+        message_body: { "#{id}": "#{message}" }.to_json
+      )
+      # TODO:
+      # resend unsuccessful messages
+    end
+    true
+  end
+
+  def send_frequent_status_updates(sleep_time = 5)
+    while true
+      status = 'Testing' # comment the lines below for development mode
+      # status = ec2.describe_instances(instance_ids: [self_id]).
+      #   reservations[0].instances[0].state.name.capitalize
+      logger.info "Send update to status board " +
+        {
+          instanceId: self_id,
+          type: 'status_update',
+          content: status
+         }.to_json
+
+      update_status_checks([self_id], status)
+      sleep sleep_time
+    end
   end
 end
